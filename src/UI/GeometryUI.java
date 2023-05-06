03345a1bb184db0e61d3f6fe9cd55f178e3cbed9
KammyKamsi <kammykamsi@gmail.com>
2023-05-06 23:14:55 +0100

    basic fix

--- src/UI/GeometryUI.java
+++ src/UI/GeometryUI.java
@@ -9,6 +9,41 @@ public class GeometryUI{
     JFrame frame;
     JButton areaOFSquare;
     JButton areaOFTrangle;
+    JButton areaOFRectangle;
+    JTextField enterLengthOFSquare;
+    JButton calculateAreaOFSquare;
+
+    public GeometryUI(){
+        frame = new JFrame("Geomtry");
+        areaOFSquare = new JButton("Area of sqaure");
+        areaOFTrangle = new JButton("Area of triangle");
+        areaOFRectangle = new JButton("Area of rectangle");
+        enterLengthOFSquare = new JTextField();
+        calculateAreaOFSquare = new JButton("calculate");
+
+
+        JPanel panel1 = new JPanel();
+        panel1.setLayout(new FlowLayout());
+        panel1.add(areaOFSquare);
+        panel1.add(enterLengthOFSquare);
+        panel1.add(calculateAreaOFSquare);
+
+        JPanel panel2 = new JPanel();
+        panel2.setLayout(new FlowLayout());
+        panel2.add(areaOFTrangle);
+
+
+        JPanel Panel3 = new JPanel();
+        Panel3.setLayout(new FlowLayout());
+        Panel3.add(areaOFRectangle);
+
+
+
+
+
+
+    }
+
 
 
 
