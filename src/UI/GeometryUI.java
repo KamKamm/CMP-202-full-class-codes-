6b2f11d18248a9e6e615c63938557b013bc2daff
KammyKamsi <kammykamsi@gmail.com>
2023-05-08 21:40:01 +0100

    still working on this

--- src/UI/GeometryUI.java
+++ src/UI/GeometryUI.java
@@ -7,19 +7,24 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import static java.awt.Color.*;
+
 public class GeometryUI{
     JFrame frame;
     JButton areaOFSquare;
     JButton areaOFTrangle;
     JButton areaOFRectangle;
+    JButton areaOFCircle;
 //    JTextField enterLengthOFSquare;
 //    JButton calculateAreaOFSquare;
 
     public GeometryUI(){
         frame = new JFrame("Geomtry");
+        frame.getContentPane().setBackground(darkGray);
         areaOFSquare = new JButton("Area of sqaure");
         areaOFTrangle = new JButton("Area of triangle");
         areaOFRectangle = new JButton("Area of rectangle");
+        areaOFCircle = new JButton("Area of circle");
 //       enterLengthOFSquare = new JTextField();
 //        calculateAreaOFSquare = new JButton("calculate");
 
@@ -39,11 +44,17 @@ public class GeometryUI{
         panel3.setLayout(new FlowLayout());
         panel3.add(areaOFRectangle);
 
+        JPanel panel4 = new JPanel();
+        panel4.setLayout(new FlowLayout());
+        panel4.add(areaOFCircle);
+
         frame.setLayout(new GridLayout(3,1));
         frame.add(panel1);
         frame.add(panel2);
         frame.add(panel3);
+        frame.add(panel4);
         frame.setSize(300,150);
+//        frame.setContentPane(darkGray);
         frame.setVisible(true);
 
         areaOFSquare.addActionListener(new ActionListener() {
@@ -68,6 +79,12 @@ public class GeometryUI{
         });
 
         areaOFTrangle.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        areaOFCircle.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
